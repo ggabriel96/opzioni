6a9f4820a2204d8234b3d991d4c4309391d1846a
Gabriel Galli <ggabriel96@hotmail.com>
2019-12-15 17:12:22 +0000

    Example of vector of ints

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <opzioni.hpp>
 
@@ -29,9 +30,16 @@ int main(int argc, char const *argv[])
         .default_value = 0,
         .implicit_value = 1
     });
+    ap.add_arg<vector<int>>({
+        .name = "--numbers",
+        .help = "A list of numbers"
+    });
 
     auto const args = ap.parse_args(argc, argv);
     cout << "\nNumber of parsed arguments: " << args.size() << nl;
     cout << "name: " << args["name"].as<string>() << nl;
     cout << "v: " << args["v"].as<int>() << nl;
+    auto const numbers = args["numbers"].as<vector<int>>();
+    cout << "numbers: " << nl;
+    for_each(begin(numbers), end(numbers), [](auto const &elem) { cout << "- " << elem << nl; });
 }
--- src/opzioni.cpp
+++ src/opzioni.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <sstream>
 
 #include <opzioni.hpp>
 
@@ -10,6 +11,20 @@ template <>
 auto convert<std::string>(std::optional<std::string> value) -> std::string
 {
     return value.value_or("");
+}
+
+// https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
+template <>
+auto convert<std::vector<int>>(std::optional<std::string> value) -> std::vector<int>
+{
+    std::vector<int> v;
+    if (!value) return v;
+    std::string token;
+    std::istringstream token_stream(value.value());
+    while (std::getline(token_stream, token, ',')) {
+        v.emplace_back(convert<int>(token));
+    }
+    return v;
 }
 
 SplitArg split_arg(std::string const &whole_arg) {
