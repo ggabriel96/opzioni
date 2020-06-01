2e5d7922fd2ef57d8236099c5018a598ad7b6e17
Gabriel Galli <ggabriel96@hotmail.com>
2020-06-01 00:26:05 -0300

    Generic floating point converter

--- examples/main.cpp
+++ examples/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char const *argv[]) {
              .help("Level of verbosity")
              .among({0, 1, 2, 3, 4})
              .with_default(0));
+  ap.add(Arg<double>("-d").help("A double"));
   ap.add(Arg<int>("--flag").help("Long flag").with_default(7).as_flag(11));
   ap.add(
       Arg<bool>("-a").help("Short flag a").with_default(false).as_flag(true));
@@ -29,6 +30,7 @@ int main(int argc, char const *argv[]) {
   fmt::print("name: {}\n", args["name"].as<std::string>());
   fmt::print("last name: {}\n", args["last-name"].as<std::string>());
   fmt::print("v: {}\n", args["v"].as<int>());
+  fmt::print("d: {}\n", args["d"].as<double>());
   fmt::print("flag: {}\n", args["flag"].as<int>());
   fmt::print("a: {}\n", args["a"].as<bool>());
   fmt::print("b: {}\n", args["b"].as<bool>());
--- include/opzioni.hpp
+++ include/opzioni.hpp
@@ -2,6 +2,7 @@
 #define OPZIONI_H
 
 #include <any>
+#include <cerrno>
 #include <charconv>
 #include <concepts>
 #include <functional>
@@ -44,6 +45,29 @@ template <Integer Int> auto convert(std::optional<std::string> arg_val) -> Int {
     return integer;
   }
   throw ConversionError("Cannot convert an empty string to int");
+}
+
+template <std::floating_point Float>
+auto convert(std::optional<std::string> arg_val) -> Float {
+  if (arg_val) {
+    char *end = nullptr;
+    Float const floatnum = [&arg_val, &end]() -> Float {
+      if constexpr (std::is_same_v<Float, float>) {
+        return std::strtof(arg_val->data(), &end);
+      } else if constexpr (std::is_same_v<Float, long double>) {
+        return std::strtold(arg_val->data(), &end);
+      } else {
+        // should I explicitly test for double and do something
+        // different in the else branch?
+        return std::strtod(arg_val->data(), &end);
+      }
+    }();
+    if (errno == ERANGE || end == arg_val->data())
+      throw ConversionError(
+          fmt::format("Could not convert `{}` to floating point", *arg_val));
+    return floatnum;
+  }
+  throw ConversionError("Cannot convert an empty string to floating point");
 }
 
 template <typename T> struct Arg {
