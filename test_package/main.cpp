602b66b57b31c68781ef5eec78570effa21f0c56
Gabriel Galli <ggabriel96@hotmail.com>
2020-06-07 20:12:45 -0300

    Forgot to update test consumer

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[]) {
   opz::ArgParser ap;
   ap.add(opz::Arg<std::string>("name").help("Your name"));
 
-  auto const args = ap.parse_args(argc, argv);
+  auto const args = ap.parse(argc, argv);
   std::cout << "Number of arguments: " << args.size() << '\n';
   std::cout << "name: " << args["name"].as<std::string>() << '\n';
 }
