c6318dc7ce342de4f7e8d971810547f12cb0f8a2
Gabriel Galli <ggabriel96@hotmail.com>
2020-09-07 12:08:57 -0300

    Moved convert for vector to header; tmp requires

--- include/converters.hpp
+++ include/converters.hpp
@@ -9,6 +9,7 @@
 #include <charconv>
 #include <concepts>
 #include <optional>
+#include <sstream>
 #include <string>
 
 #include <fmt/format.h>
@@ -45,6 +46,21 @@ template <std::floating_point Float> auto convert(std::string arg_val) -> Float 
   if (errno == ERANGE || end == arg_val.data())
     throw ConversionError(fmt::format("Could not convert `{}` to a floating point type", arg_val));
   return floatnum;
+}
+
+// https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
+template <typename Container>
+auto convert(std::string value) -> Container
+    requires(std::is_same_v<Container, std::vector<typename Container::value_type>>) {
+  Container container;
+  if (value.empty())
+    return container;
+  std::string token;
+  std::istringstream token_stream(value);
+  while (std::getline(token_stream, token, ',')) {
+    container.emplace_back(convert<typename Container::value_type>(token));
+  }
+  return container;
 }
 
 } // namespace opzioni
