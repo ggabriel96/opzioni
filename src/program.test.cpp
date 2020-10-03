131e3abb80034b96c44b5d40634a218402e11d1c
Gabriel Galli <ggabriel96@hotmail.com>
2020-10-03 20:35:40 -0300

    basic argument specification test scenario

--- src/program.test.cpp
+++ src/program.test.cpp
@@ -1,9 +1,85 @@
 #include "program.hpp"
 
 #include <array>
+#include <optional>
 #include <string>
 
 #include <catch2/catch.hpp>
+
+SCENARIO("argument specification") {
+  using namespace opzioni;
+  using namespace std::string_literals;
+  using Catch::Matchers::Message;
+
+  GIVEN("an empty program specification") {
+    Program program;
+
+    WHEN("user specifies a positional argument") {
+      auto const positional_name = "positional"s;
+      auto const &inserted_positional = program.pos(positional_name);
+
+      THEN("it is added to the specification, is marked required, and its address is returned") {
+        REQUIRE(program.positionals.size() == 1);
+        REQUIRE(program.options.size() == 0);
+        REQUIRE(program.flags.size() == 0);
+        REQUIRE(&inserted_positional == &program.positionals[0]);
+
+        REQUIRE(inserted_positional.name == positional_name);
+        REQUIRE(inserted_positional.description == ""s);
+        REQUIRE(inserted_positional.is_required);
+        REQUIRE(inserted_positional.default_value == std::nullopt);
+        REQUIRE(inserted_positional.set_value == std::monostate{});
+        REQUIRE(inserted_positional.act ==
+                static_cast<actions::signature<ArgumentType::POSITIONAL>>(actions::assign<std::string>));
+        REQUIRE(inserted_positional.gather_n.amount == 1);
+      }
+    }
+
+    WHEN("user specifies an option") {
+      auto const option_name = "option"s;
+      auto const &inserted_option = program.opt(option_name);
+
+      THEN("it is added to the specification and its address is returned") {
+        REQUIRE(program.positionals.size() == 0);
+        REQUIRE(program.options.size() == 1);
+        REQUIRE(program.flags.size() == 0);
+        REQUIRE(&inserted_option == &program.options[option_name]);
+
+        REQUIRE(inserted_option.name == option_name);
+        REQUIRE(inserted_option.description == ""s);
+        REQUIRE(!inserted_option.is_required);
+        REQUIRE(inserted_option.default_value == std::nullopt);
+        REQUIRE(inserted_option.set_value == std::nullopt);
+        REQUIRE(inserted_option.act ==
+                static_cast<actions::signature<ArgumentType::OPTION>>(actions::assign<std::string>));
+        REQUIRE(inserted_option.gather_n.amount == 1);
+      }
+    }
+
+    WHEN("user specifies a flag") {
+      auto const flag_name = "flag"s;
+      auto const &inserted_flag = program.flag(flag_name);
+
+      THEN("it is added to the specification, set_value is set to true, action is set to assign<bool>, and its "
+           "address is returned") {
+        REQUIRE(program.positionals.size() == 0);
+        REQUIRE(program.options.size() == 0);
+        REQUIRE(program.flags.size() == 1);
+        REQUIRE(&inserted_flag == &program.flags[flag_name]);
+
+        REQUIRE(inserted_flag.name == flag_name);
+        REQUIRE(inserted_flag.description == ""s);
+        REQUIRE(!inserted_flag.is_required);
+        REQUIRE(inserted_flag.default_value == std::nullopt);
+        REQUIRE(inserted_flag.set_value.has_value());
+        REQUIRE(std::holds_alternative<bool>(*inserted_flag.set_value));
+        REQUIRE(std::get<bool>(inserted_flag.set_value.value()) == true);
+        REQUIRE(inserted_flag.act == static_cast<actions::signature<ArgumentType::FLAG>>(actions::assign<bool>));
+        REQUIRE(inserted_flag.gather_n == std::monostate{});
+      }
+    }
+  }
+}
 
 SCENARIO("positional arguments") {
   using Catch::Matchers::Message;
