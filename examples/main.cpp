f6e67b7f7936d0a3ad4843a9e3e22c0457645193
Gabriel Galli <ggabriel96@hotmail.com>
2020-11-02 21:00:40 +0000

    Categorized arguments from example

--- examples/main.cpp
+++ examples/main.cpp
@@ -24,25 +24,31 @@ int main(int argc, char const *argv[]) {
       .help("The equivalent of Python's argparse `nargs`, consumes all integers into a vector")
       .gather<int>()
       .otherwise(std::vector{-1});
-  program.opt("last-name").help("Your last name");
-  program.opt("verbosity", 'v').help("Level of verbosity").set(1).otherwise(0);
+
   program.opt("double", 'd').help("A double").otherwise(7.11);
-  program.flag("flag", 'f')
-      .set("do something!"s)
-      .otherwise("nope"s)
-      .help("The equivalent of Python's argparse `store_const`");
-  program.opt("o").help("Testing an option with only a short name").otherwise("oh"s);
-  program.flag("t").help("Testing a flag with only a short name").otherwise(false);
+  program.opt("last-name").help("Your last name");
+  program.opt("o")
+      .help("Showing how bad is an option with only a short name. The `--` is intentionally kept")
+      .otherwise("oh"s);
+  program.opt("num", 'n').help("A list of numbers").action(append<int>);
+  program.opt("str").help("Appends to a list of strings").action(append<std::string>).otherwise(std::vector{""s});
+  program.opt("vec")
+      .otherwise(std::vector<int>{})
+      .help("Illustrating the difference between appending and assigning to a vector");
+  program.opt("verbosity", 'v').help("Level of verbosity").set(1).otherwise(0);
+
   program.flag("append", 'a')
       .set(1)
       .otherwise(std::vector{-1})
       .help("The equivalent of Python's argparse `append_const`")
       .action(append<int>);
-  program.opt("num", 'n').help("A list of numbers").action(append<int>);
-  program.opt("vec")
-      .otherwise(std::vector<int>{})
-      .help("Illustrating the difference between appending and assigning to a vector");
-  program.opt("str").help("Appends to a list of strings").action(append<std::string>).otherwise(std::vector{""s});
+  program.flag("flag", 'f')
+      .set("do something!"s)
+      .otherwise("nope"s)
+      .help("The equivalent of Python's argparse `store_const`");
+  program.flag("t")
+      .help("Showing how bad is a flag with only a short name. The `--` is intentionally kept")
+      .otherwise(false);
 
   auto &subcmd = program.cmd("subcmd").help("Just showing how subcommands work");
   subcmd.pos("subname").help("Your name again, please");
@@ -53,18 +59,21 @@ int main(int argc, char const *argv[]) {
   auto const args = program(argc, argv);
   print("\nCommand name: {}\n", args.cmd_name);
   print("Number of arguments: {}\n", args.size());
+
   print("name: {}\n", args.as<std::string>("name"));
   print("gather-ints: [{}]\n", fmt::join(args.as<std::vector<int>>("gather-ints"), ", "));
-  print("last-name: {}\n", args.has("last-name"s) ? args.as<std::string>("last-name") : "no last name");
-  print("verbosity: {}\n", args.as<int>("verbosity"));
-  print("t: {}\n", args.as<bool>("t"));
-  print("o: {}\n", args.as<std::string>("o"));
+
   print("double: {}\n", args.as<double>("double"));
-  print("flag: {}\n", args.as<std::string>("flag"));
-  print("append: {}\n", args.as<std::vector<int>>("append"));
+  print("last-name: {}\n", args.has("last-name"s) ? args.as<std::string>("last-name") : "no last name");
+  print("o: {}\n", args.as<std::string>("o"));
   print("num: [{}]\n", fmt::join(args.as<std::vector<int>>("num"), ", "));
-  print("vec: [{}]\n", fmt::join(args.as<std::vector<int>>("vec"), ", "));
   print("str: [{}]\n", fmt::join(args.as<std::vector<std::string>>("str"), ", "));
+  print("vec: [{}]\n", fmt::join(args.as<std::vector<int>>("vec"), ", "));
+  print("verbosity: {}\n", args.as<int>("verbosity"));
+
+  print("append: {}\n", args.as<std::vector<int>>("append"));
+  print("flag: {}\n", args.as<std::string>("flag"));
+  print("t: {}\n", args.as<bool>("t"));
 
   if (args.subcmd != nullptr) {
     auto subargs = *args.subcmd;
