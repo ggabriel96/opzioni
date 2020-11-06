2c31ed0c46666f83eb9d38f68a3962cebaddd433
Gabriel Galli <ggabriel96@hotmail.com>
2020-11-06 02:51:16 +0000

    Fix limit_within bug

--- include/opzioni.hpp
+++ include/opzioni.hpp
@@ -332,9 +332,9 @@ public:
     std::size_t cur_max = max_width - margin_left;
     std::vector<std::vector<std::string_view>> lines(1);
     for (auto const &word : words) {
-      if (word.length() <= cur_max) {
+      if (word.length() < cur_max) {
         lines.back().push_back(word);
-        cur_max -= (word.length() + (word.length() < cur_max)); // +1 for space in between if not last word
+        cur_max -= (word.length() + 1); // +1 for space in between
       } else {
         lines.push_back({word});
         cur_max = max_width - margin_left - word.length();
