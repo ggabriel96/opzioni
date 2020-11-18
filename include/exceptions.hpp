c01356e924295fa2b90dae7026af500fd29e06fb
Gabriel Galli <ggabriel96@hotmail.com>
2020-11-18 02:22:18 +0000

    Custom base exceptions; first draft of UserError inheritance

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -5,24 +5,35 @@
 
 namespace opzioni {
 
-class ConversionError : public std::domain_error {
-  using std::domain_error::domain_error;
+// Base class for exceptions thrown because of errors from the users of our library
+class ConsumerError : public std::logic_error {
+  using std::logic_error::logic_error;
 };
 
-class MissingValue : public std::invalid_argument {
-  using std::invalid_argument::invalid_argument;
+// Base class for exceptions thrown because of errors from the users of the CLI program
+class UserError : public std::runtime_error {
+  using std::runtime_error::runtime_error;
 };
 
+class ConversionError : public UserError {
+  using UserError::UserError;
+};
+
+class MissingValue : public UserError {
+  using UserError::UserError;
+};
+
+struct DuplicateAssignment : public UserError {
+  using UserError::UserError;
+};
+
+// has mixed usage, will fix later...
 class UnknownArgument : public std::out_of_range {
   using std::out_of_range::out_of_range;
 };
 
 class ArgumentAlreadyExists : public std::logic_error {
   using std::logic_error::logic_error;
-};
-
-struct DuplicateAssignment : std::invalid_argument {
-  using std::invalid_argument::invalid_argument;
 };
 
 } // namespace opzioni
