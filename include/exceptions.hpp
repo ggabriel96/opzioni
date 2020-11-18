54ddbaba243d71f31fcb6da0868c35e8a77ee25a
Gabriel Galli <ggabriel96@hotmail.com>
2020-11-18 02:16:01 +0000

    Removed unused exception classes

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -5,31 +5,11 @@
 
 namespace opzioni {
 
-class MissingRequiredArgument : public std::out_of_range {
-  using std::out_of_range::out_of_range;
-};
-
 class ConversionError : public std::domain_error {
   using std::domain_error::domain_error;
 };
 
-class FlagHasValue : public std::invalid_argument {
-  using std::invalid_argument::invalid_argument;
-};
-
-class InvalidArgument : public std::invalid_argument {
-  using std::invalid_argument::invalid_argument;
-};
-
-class InvalidChoice : public std::out_of_range {
-  using std::out_of_range::out_of_range;
-};
-
 class MissingValue : public std::invalid_argument {
-  using std::invalid_argument::invalid_argument;
-};
-
-class ParseError : public std::invalid_argument {
   using std::invalid_argument::invalid_argument;
 };
 
@@ -39,10 +19,6 @@ class UnknownArgument : public std::out_of_range {
 
 class ArgumentAlreadyExists : public std::logic_error {
   using std::logic_error::logic_error;
-};
-
-class TooManyDashes : public std::invalid_argument {
-  using std::invalid_argument::invalid_argument;
 };
 
 struct DuplicateAssignment : std::invalid_argument {
