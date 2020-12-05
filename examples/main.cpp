fa907fd2a30ee3d471214c6ad50303c2e5a6bd97
Gabriel Galli <ggabriel96@hotmail.com>
2020-12-05 05:28:10 +0000

    Improved strings for main example too

--- examples/main.cpp
+++ examples/main.cpp
@@ -15,8 +15,8 @@ int main(int argc, char const *argv[]) {
   auto program =
       opzioni::Program("opzioni's main example")
           .intro("A short example file illustrating opzioni's simpler features")
-          .details("This example only covers simple positionals, options, and flags. For examples of more "
-                   "complicated parse actions or subcommands, please take a look at the other examples.")
+          .details("This example only covers simple positionals, options, and flags. For examples of more"
+                   " complicated parse actions or subcommands, please take a look at the other examples.")
           .auto_help() +
       pos("name").help("Your first name") + opt("double", "d").help("A double").otherwise(7.11) +
       opt("last-name").help("Your last name") +
@@ -25,22 +25,20 @@ int main(int argc, char const *argv[]) {
       opt("str").help("Appends to a list of strings").action(append<std::string>).otherwise(std::vector{""s}) +
       opt("vec")
           .otherwise(std::vector<int>{})
-          .help("This option uses the default action, so it'll not append to a list from each appearence in the "
-                "CLI. It "
-                "will instead parse a comma-separated list once") +
+          .help("This option uses the default action, so it'll not append to a list from each appearence in the CLI."
+                " It will instead parse a comma-separated list once") +
       opt("verbose", "v").help("Level of verbosity").set(1).otherwise(0) +
       flg("append", "a")
           .set(1)
           .otherwise(std::vector{-1})
-          .help("The equivalent of Python's argparse `append_const`: will append the defined value every time it "
-                "appears "
-                "in the CLI")
+          .help("The equivalent of Python's argparse `append_const`:"
+                " will append the defined value every time it appears in the CLI")
           .action(append<int>) +
       flg("flag", "f")
           .set("do something!"s)
           .otherwise("nope"s)
-          .help("The equivalent of Python's argparse `store_const`: will store the defined value if it appears "
-                "in the CLI") +
+          .help("The equivalent of Python's argparse `store_const`:"
+                " will store the defined value if it appears in the CLI") +
       flg("t").help("We also support flags with only short names").otherwise(false);
 
   auto const args = program(argc, argv);
