fa2e0c13da93814f715c57d619b4605ef4eb3f68
Gabriel Galli <ggabriel96@hotmail.com>
2020-12-12 18:39:06 -0300

    Little improvements to vector<int> case

--- src/converters.test.cpp
+++ src/converters.test.cpp
@@ -1,30 +1,46 @@
 #include <optional>
-#include <string>
+#include <string_view>
 #include <vector>
 
 #include <catch2/catch.hpp>
 
 #include "converters.hpp"
 
-SCENARIO("provided converter for vector<int>", "[vector]") {
+SCENARIO("vector<int>", "[vector]") {
   using vec = std::vector<int>;
-  using namespace std::string_literals;
+  using namespace std::string_view_literals;
 
   GIVEN("a proper comma-separated list of integers") {
-    auto const input = "1,2,3"s;
+    auto const input = "1,2,3"sv;
 
-    WHEN("converting the list") {
+    WHEN("calling convert") {
       auto const result = opzioni::convert<vec>(input);
-      REQUIRE(result == vec{1, 2, 3});
+
+      THEN("we should get a vector of int of that list") {
+        REQUIRE(result == vec{1, 2, 3});
+      }
     }
   }
 
   GIVEN("a comma-separated list of integers with a trailing comma") {
-    auto const input = "1,2,3,"s;
+    auto const input = "1,2,3,"sv;
 
-    WHEN("converting the list") {
+    WHEN("calling convert") {
       auto const result = opzioni::convert<vec>(input);
-      REQUIRE(result == vec{1, 2, 3});
+
+      THEN("we should get a vector of int of that list") {
+        REQUIRE(result == vec{1, 2, 3});
+      }
+    }
+  }
+
+  GIVEN("a comma-separated list of integers with a leading comma") {
+    auto const input = ",1,2,3"sv;
+
+    WHEN("calling convert") {
+      THEN("we should throw an error") {
+        REQUIRE_THROWS_AS(opzioni::convert<vec>(input), opzioni::ConversionError);
+      }
     }
   }
 }
