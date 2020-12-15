b08bd81ab2d9ac3162aabb8345d9905893b924c3
Gabriel Galli <ggabriel96@hotmail.com>
2020-12-14 22:31:18 -0300

    Updated test_package

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <string>
+#include <string_view>
 
 #include <opzioni.hpp>
 
 int main(int argc, char const *argv[]) {
-  opzioni::Program program;
-  program.add(opzioni::Arg<std::string>("name").help("Your name"));
+  using opzioni::Program, opzioni::Pos;
 
-  auto const args = program.parse(argc, argv);
+  auto program = Program("test-package") + Pos("name").help("Your name");
+
+  auto const args = program(argc, argv);
   std::cout << "Number of arguments: " << args.size() << '\n';
-  std::cout << "name: " << args["name"].as<std::string>() << '\n';
+  std::cout << "name: " << args["name"].as<std::string_view>() << '\n';
 }
