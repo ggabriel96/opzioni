326b738bded75063c4e30e90d305923e1322eaff
Gabriel Galli <ggabriel96@hotmail.com>
2020-12-19 16:53:20 -0300

    Little improvements to main example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <string_view>
 #include <vector>
 
 #include <fmt/format.h>
@@ -8,30 +8,27 @@
 
 int main(int argc, char const *argv[]) {
   using fmt::print;
-  using opzioni::actions::append;
   using namespace std::string_view_literals;
-  using opzioni::Flg, opzioni::Opt, opzioni::Pos;
+  using opzioni::Program, opzioni::Flg, opzioni::Opt, opzioni::Pos;
 
   auto program =
-      opzioni::Program("main")
-          .intro("A short example file illustrating opzioni's simpler features")
+      Program("main")
+          .intro("A short example illustrating opzioni's simpler features")
           .details("This example only covers simple positionals, options, and flags. For examples of more"
                    " complicated parse actions or subcommands, please take a look at the other examples.")
           .auto_help() +
       Pos("name").help("Your first name") + Opt("double", "d").help("A double").otherwise(7.11) +
       Opt("last-name").help("Your last name") +
       Opt("o").help("We also support options with only short names").otherwise("oh"sv) +
-      Opt("num", "n").help("Creates a list of numbers with each appearence of this argument").append<int>() +
-      Opt("str").help("Appends to a list of strings").append<std::string_view>() +
-      Opt("vec").csv_of<int>().help(
-          "This option uses the default action, so it'll not append to a list from each appearence in the CLI."
-          " It will instead parse a comma-separated list once") +
+      Opt("num", "n").append<int>().help("Creates a vector of numbers with each appearence of this argument") +
+      Opt("csv").csv_of<int>().help("In contrast to `append`, this will create a vector of numbers from a single "
+                                    "comma-separated list of values") +
       Opt("verbose", "v").help("Level of verbosity").set(1).otherwise(0) +
       Flg("append", "a")
           .set(1)
+          .append<int>()
           .help("The equivalent of Python's argparse `append_const`:"
-                " will append the defined value every time it appears in the CLI")
-          .append<int>() +
+                " will append the defined value every time it appears in the CLI") +
       Flg("flag", "f")
           .set("do something!"sv)
           .otherwise("nope"sv)
@@ -46,11 +43,11 @@ int main(int argc, char const *argv[]) {
   print("name: {}\n", args.as<std::string_view>("name"));
 
   print("double: {}\n", args.as<double>("double"));
-  print("last-name: {}\n", args.has("last-name"sv) ? args.as<std::string_view>("last-name") : "no last name");
+  // illustrating how one could handle arguments without default values
+  print("last-name: {}\n", args.has("last-name") ? args.as<std::string_view>("last-name") : "no last name");
   print("o: {}\n", args.as<std::string_view>("o"));
   print("num: {}\n", args.as<std::vector<int>>("num"));
-  print("str: {}\n", args.as<std::vector<std::string_view>>("str"));
-  print("vec: {}\n", args.as<std::vector<int>>("vec"));
+  print("csv: {}\n", args.as<std::vector<int>>("csv"));
   print("verbose: {}\n", args.as<int>("verbose"));
 
   print("{}\n", args.has("append"));
