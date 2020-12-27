4ee0343bbdd24bbf95a727dd2ec98b4c86d857d8
Gabriel Galli <ggabriel96@hotmail.com>
2020-12-27 20:01:57 +0000

    Testing insertion with mixed arg types

--- src/opzioni.program.test.cpp
+++ src/opzioni.program.test.cpp
@@ -50,5 +50,25 @@ SCENARIO("adding arguments", "[Program]") {
         REQUIRE(program.args()[4].name == "pos3");
       }
     }
+
+    WHEN("other arguments are added before positionals") {
+      program + Flg("flg1") * Opt("opt2") * Pos("pos3") * Pos("pos1") * Pos("pos2") * Opt("opt1") * Flg("flg2");
+
+      REQUIRE(program.args().size() == 7);
+      THEN("positionals_amount should be incremented") { REQUIRE(program.positionals_amount == 3); }
+      THEN("positionals should be added as first arguments, but preserving the order of insertion") {
+        REQUIRE(program.args()[0].name == "pos3");
+        REQUIRE(program.args()[1].name == "pos1");
+        REQUIRE(program.args()[2].name == "pos2");
+      }
+      THEN("the rest of the arguments should be sorted lexicographically by name") {
+        // also using is_sorted to check `operator<`
+        REQUIRE(std::is_sorted(std::next(program.args().begin(), program.positionals_amount), program.args().end()));
+        REQUIRE(program.args()[3].name == "flg1");
+        REQUIRE(program.args()[4].name == "flg2");
+        REQUIRE(program.args()[5].name == "opt1");
+        REQUIRE(program.args()[6].name == "opt2");
+      }
+    }
   }
 }
