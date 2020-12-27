1e2bfad8f3bdb1ec80d687446e55c93cb16fac13
Gabriel Galli <ggabriel96@hotmail.com>
2020-12-27 19:56:20 +0000

    Added a few THEN sections

--- src/opzioni.program.test.cpp
+++ src/opzioni.program.test.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 
 #include <catch2/catch.hpp>
@@ -11,36 +12,43 @@ SCENARIO("adding arguments", "[Program]") {
   GIVEN("an empty Program") {
     Program program;
 
-    REQUIRE(program.args().size() == 0);
-    REQUIRE(program.positionals_amount == 0);
+    THEN("it should initially have no arguments") {
+      REQUIRE(program.args().size() == 0);
+      REQUIRE(program.positionals_amount == 0);
+    }
 
     WHEN("a positional is added") {
       program + std::array{Pos("pos")};
 
       REQUIRE(program.args().size() == 1);
-      REQUIRE(program.positionals_amount == 1);
-      REQUIRE(program.args()[0].name == "pos");
+
+      THEN("positionals_amount should be incremented") { REQUIRE(program.positionals_amount == 1); }
+      THEN("it should be added as first argument") { REQUIRE(program.args()[0].name == "pos"); }
     }
 
     WHEN("two positionals are added") {
       program + Pos("pos1") * Pos("pos2");
 
       REQUIRE(program.args().size() == 2);
-      REQUIRE(program.positionals_amount == 2);
-      REQUIRE(program.args()[0].name == "pos1");
-      REQUIRE(program.args()[1].name == "pos2");
+      THEN("positionals_amount should be incremented") { REQUIRE(program.positionals_amount == 2); }
+      THEN("they should be added as first arguments") {
+        REQUIRE(program.args()[0].name == "pos1");
+        REQUIRE(program.args()[1].name == "pos2");
+      }
     }
 
     WHEN("multiple positionals are added") {
       program + Pos("pos5") * Pos("pos2") * Pos("pos4") * Pos("pos1") * Pos("pos3");
 
       REQUIRE(program.args().size() == 5);
-      REQUIRE(program.positionals_amount == 5);
-      REQUIRE(program.args()[0].name == "pos5");
-      REQUIRE(program.args()[1].name == "pos2");
-      REQUIRE(program.args()[2].name == "pos4");
-      REQUIRE(program.args()[3].name == "pos1");
-      REQUIRE(program.args()[4].name == "pos3");
+      THEN("positionals_amount should be incremented") { REQUIRE(program.positionals_amount == 5); }
+      THEN("they should be added as first arguments") {
+        REQUIRE(program.args()[0].name == "pos5");
+        REQUIRE(program.args()[1].name == "pos2");
+        REQUIRE(program.args()[2].name == "pos4");
+        REQUIRE(program.args()[3].name == "pos1");
+        REQUIRE(program.args()[4].name == "pos3");
+      }
     }
   }
 }
