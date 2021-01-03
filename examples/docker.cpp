5acd719e8747dc854683894eb43bd58cf2da403a
Gabriel Galli <ggabriel96@hotmail.com>
2021-01-03 16:40:26 +0000

    Specifying the defaults for docker

--- examples/docker.cpp
+++ examples/docker.cpp
@@ -48,18 +48,24 @@ int main(int argc, char const *argv[]) {
           .v("version 20.10.1, build 831ebea")
           .intro("A self-sufficient runtime for containers")
           .details("Run 'docker COMMAND --help' for more information on a command.") +
-      Help() * Version() * Opt("config").help("Location of client config files (default \"~/.docker\")") *
+      Help() * Version() *
+          Opt("config").help("Location of client config files (default \"~/.docker\")").otherwise("~/.docker") *
           Opt("context", "c")
               .help("Name of the context to use to connect to the daemon (overrides DOCKER_HOST env var and default "
                     "context set with \"docker context use\")") *
           Flg("debug", "D").help("Enable debug mode") *
           Opt("host", "H").help("Daemon socket(s) to connect to").append() *
           Opt("log-level", "l")
-              .help("Set the logging level (\"debug\"|\"info\"|\"warn\"|\"error\"|\"fatal\") (default \"info\")") *
+              .help("Set the logging level (\"debug\"|\"info\"|\"warn\"|\"error\"|\"fatal\") (default \"info\")")
+              .otherwise("info") *
           Flg("tls").help("Use TLS; implied by --tlsverify") *
-          Opt("tlscacert").help("Trust certs signed only by this CA (default \"~/.docker/ca.pem\")") *
-          Opt("tlscert").help("Path to TLS certificate file (default \"~/.docker/cert.pem\")") *
-          Opt("tlskey").help("Path to TLS key file (default \"~/.docker/key.pem\")") *
+          Opt("tlscacert")
+              .help("Trust certs signed only by this CA (default \"~/.docker/ca.pem\")")
+              .otherwise("~/.docker/ca.pem") *
+          Opt("tlscert")
+              .help("Path to TLS certificate file (default \"~/.docker/cert.pem\")")
+              .otherwise("~/.docker/cert.pem") *
+          Opt("tlskey").help("Path to TLS key file (default \"~/.docker/key.pem\")").otherwise("~/.docker/key.pem") *
           Flg("tlsverify").help("Use TLS and verify the remote") +
       Cmd(exec) + Cmd(pull);
 
