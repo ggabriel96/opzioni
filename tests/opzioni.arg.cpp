13b608c7104481e91eb7d50875c72f59d83a73e3
Gabriel Galli <ggabriel96@hotmail.com>
2021-01-09 20:15:47 +0000

    Opt function tests

--- tests/opzioni.arg.cpp
+++ tests/opzioni.arg.cpp
@@ -36,4 +36,34 @@ SCENARIO("default values", "[Arg][defaults]") {
     THEN("gather_amount should be 1") { REQUIRE(arg.gather_amount == 1); }
     THEN("default_setter should be nullptr") { REQUIRE(arg.default_setter == nullptr); }
   }
+
+  WHEN("Opt is called with one argument") {
+    constexpr auto arg = Opt("opt");
+
+    THEN("type should be OPT") { REQUIRE(arg.type == ArgType::OPT); }
+    THEN("name should be equal to argument") { REQUIRE(arg.name == "opt"); }
+    THEN("abbrev should be empty") { REQUIRE(arg.abbrev.empty()); }
+    THEN("description should be empty") { REQUIRE(arg.description.empty()); }
+    THEN("is_required should be false") { REQUIRE(!arg.is_required); }
+    THEN("default_value should be empty string") { REQUIRE(std::get<std::string_view>(arg.default_value) == ""); }
+    THEN("set_value should be empty") { REQUIRE(std::holds_alternative<std::monostate>(arg.set_value)); }
+    THEN("action_fn should be assign<string_view>") { REQUIRE(arg.action_fn == actions::assign<std::string_view>); }
+    THEN("gather_amount should be 1") { REQUIRE(arg.gather_amount == 1); }
+    THEN("default_setter should be nullptr") { REQUIRE(arg.default_setter == nullptr); }
+  }
+
+  WHEN("Opt is called with two arguments") {
+    constexpr auto arg = Opt("opt", "o");
+
+    THEN("type should be OPT") { REQUIRE(arg.type == ArgType::OPT); }
+    THEN("name should be equal to first argument") { REQUIRE(arg.name == "opt"); }
+    THEN("abbrev should be equal to second argument") { REQUIRE(arg.abbrev == "o"); }
+    THEN("description should be empty") { REQUIRE(arg.description.empty()); }
+    THEN("is_required should be false") { REQUIRE(!arg.is_required); }
+    THEN("default_value should be empty string") { REQUIRE(std::get<std::string_view>(arg.default_value) == ""); }
+    THEN("set_value should be empty") { REQUIRE(std::holds_alternative<std::monostate>(arg.set_value)); }
+    THEN("action_fn should be assign<string_view>") { REQUIRE(arg.action_fn == actions::assign<std::string_view>); }
+    THEN("gather_amount should be 1") { REQUIRE(arg.gather_amount == 1); }
+    THEN("default_setter should be nullptr") { REQUIRE(arg.default_setter == nullptr); }
+  }
 }
