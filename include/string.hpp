cf93cab097d52314bfe9fa5c6b3c9042b1a19a6a
Gabriel Galli <ggabriel96@hotmail.com>
2021-01-17 22:34:29 -0300

    helpers for name validation in string.hpp

--- include/string.hpp
+++ include/string.hpp
@@ -1,6 +1,7 @@
 #ifndef OPZIONI_STRING_H
 #define OPZIONI_STRING_H
 
+#include <algorithm>
 #include <concepts>
 #include <string>
 #include <string_view>
@@ -10,6 +11,7 @@ namespace opzioni {
 
 constexpr char nl = '\n';
 constexpr std::string_view whitespace = " \f\n\r\t\v";
+constexpr std::string_view valid_name_chars = "0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ-abcdefghijklmnopqrstuvwxyz";
 
 std::string_view trim(std::string_view) noexcept;
 
@@ -34,6 +36,14 @@ auto limit_within(std::ranges::range auto const &words, std::size_t const max_wi
 auto limit_within(std::string_view const, std::size_t const) noexcept -> std::vector<std::vector<std::string_view>>;
 
 std::string limit_string_within(std::string_view const, std::size_t const) noexcept;
+
+constexpr bool contains(std::string_view const str, char const ch) noexcept {
+  return str.find(ch) != std::string_view::npos;
+}
+
+constexpr bool is_valid_name(std::string_view name) noexcept {
+  return std::ranges::find_if(name, [](char const ch) { return !contains(valid_name_chars, ch); }) == name.end();
+}
 
 } // namespace opzioni
 
