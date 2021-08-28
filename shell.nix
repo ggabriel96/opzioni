ddd0037b13bd2a8114a911612bdd63a97a1dae41
Gabriel Galli <ggabriel96@hotmail.com>
2021-08-28 17:32:57 -0300

    build: remove files related to Conan, Docker, and Nix

--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz") {} }:
-
-pkgs.gcc10Stdenv.mkDerivation {
-  name = "opzioni";
-  buildInputs = with pkgs; [ clang-tools cmake conan meson ninja pkg-config-unwrapped ];
-}
