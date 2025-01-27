af2ba80bcd9c7df20b7ba29d08cb833a68e7a199
Gabriel Galli <ggabriel96@hotmail.com>
2025-01-27 02:41:59 -0300

    feat: add IndexOfStr

--- include/get_type.hpp
+++ include/get_type.hpp
@@ -18,6 +18,23 @@ struct InStringList<Needle, StringList<Needle, Haystack...>> : std::true_type {}
 template <fixed_string Needle, fixed_string Other, fixed_string... Haystack>
 struct InStringList<Needle, StringList<Other, Haystack...>> : InStringList<Needle, StringList<Haystack...>> {};
 
+// base case (str not found)
+template<int Idx, fixed_string, typename...>
+struct IndexOfStr : std::integral_constant<int, -1> {};
+
+// base case (str found)
+template<int Idx, fixed_string Needle, fixed_string... Haystack>
+struct IndexOfStr<Idx, Needle, StringList<Needle, Haystack...>> : std::integral_constant<int, Idx> {};
+
+// recursive case (keep looking)
+template<int Idx, fixed_string Needle, fixed_string Other, fixed_string... Haystack>
+struct IndexOfStr<Idx, Needle, StringList<Other, Haystack...>> : IndexOfStr<Idx + 1, Needle, StringList<Haystack...>> {};
+
+// +------------------------------+
+// |           TypeList           |
+// +------------------------------+
+
+
 template <typename...>
 struct TypeList;
 
