51484842be61b57bc0d5b5d4691b4bbb9ff8703b
Gabriel Galli <ggabriel96@hotmail.com>
2025-01-27 02:41:31 -0300

    fix: simplify fixed_string

--- include/fixed_string.hpp
+++ include/fixed_string.hpp
@@ -3,23 +3,15 @@
 
 #include <cstddef>
 
-struct construct_from_pointer_t {};
-constexpr auto construct_from_pointer = construct_from_pointer_t{};
-
 template <size_t N>
 struct fixed_string {
   char data[N];
 
-  constexpr fixed_string(char const input[N], construct_from_pointer_t) {
+  constexpr fixed_string(char const (&input)[N]) {
     for (size_t i{0}; i < N; ++i) {
       data[i] = input[i];
     }
   }
-
-  constexpr fixed_string(char const (&input)[N + 1]) noexcept : fixed_string{input, construct_from_pointer} {}
 };
-
-template <size_t N>
-fixed_string(char const (&)[N]) -> fixed_string<N - 1>;
 
 #endif
