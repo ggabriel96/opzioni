4ea81b6e65893d671ff1bad4d97022e1cf9582b3
Gabriel Galli <ggabriel96@hotmail.com>
2025-02-02 23:43:43 -0300

    build: add parsing files

--- include/experimental/all.hpp
+++ include/experimental/all.hpp
@@ -1,8 +1,10 @@
 #ifndef OPZIONI_EXPERIMENTAL_ALL_H
 #define OPZIONI_EXPERIMENTAL_ALL_H
 
+#include "experimental/arg.hpp"
 #include "experimental/fixed_string.hpp"
 #include "experimental/get_type.hpp"
+#include "experimental/parsing.hpp"
 #include "experimental/program.hpp"
 #include "experimental/string_list.hpp"
 #include "experimental/type_list.hpp"
