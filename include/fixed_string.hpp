27bde8c0f94ca7555e8b589b57d205328556dd9a
Gabriel Galli <ggabriel96@hotmail.com>
2025-03-04 21:00:20 -0300

    fix: store the trailing \0 and keep N - 1
    Without the trailing the \0, sometimes the compiler prints the strings as if arrays of int8.

--- include/fixed_string.hpp
+++ include/fixed_string.hpp
@@ -8,15 +8,17 @@ namespace opz {
 
 template <size_t N>
 struct FixedString {
-  char data[N];
+  char data[N + 1]; // hold space for '\0'
   std::size_t size = N;
 
   constexpr FixedString(char const *input) noexcept {
     for (size_t i{0}; i < N; ++i) {
       data[i] = input[i];
     }
+    data[N] = '\0';
   }
 
+  // string_view does not generally store the trailing '\0'
   constexpr operator std::string_view() const noexcept { return {data, N}; }
 };
 
