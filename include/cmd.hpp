9e77dfc0df0b1a2345eba45189e955dfbea51e88
Gabriel Galli <ggabriel96@hotmail.com>
2025-03-26 16:20:47 -0300

    fix: remove commented-out code

--- include/cmd.hpp
+++ include/cmd.hpp
@@ -132,23 +132,6 @@ struct Cmd<StringList<Names...>, TypeList<Types...>, SubCmds...> {
   consteval auto flg(ArgMeta<T> meta) {
     return flg<Name, "", T>(meta);
   }
-
-  // template<FixedString Name>
-  // constexpr std::optional<typename GetType<Name, arg_names, arg_types>::type> GetValue() const noexcept {
-  //   using ValueIdx = IndexOfStr<0, Name, arg_names>;
-  //   static_assert(ValueIdx::value != -1, "unknown parameter name");
-  //   return std::get<ValueIdx::value>(values);
-  // }
-
-  // template<FixedString Name, typename V>
-  // void SetValue(V value) noexcept {
-  //   using T = GetType<Name, arg_names, arg_types>::type;
-  //   static_assert(!std::is_same_v< T, void >, "unknown parameter name");
-  //   static_assert(std::is_same_v< V, T >, "parameter of given name has different type than provided value");
-  //   using ValueIdx = IndexOfType<0, V, arg_types>;
-
-  //   std::get<ValueIdx::value>(values).emplace(value);
-  // }
 };
 
 consteval auto new_cmd(std::string_view name, std::string_view version = "") {
