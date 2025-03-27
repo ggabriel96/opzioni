7911b624fee538474b7e8f74147d917dc8423147
Gabriel Galli <ggabriel96@hotmail.com>
2025-03-27 01:17:01 -0300

    feat: add hello example

---
+++ examples/hello.cpp
@@ -0,0 +1,16 @@
+#include <print>
+#include <variant>
+
+#include "opzioni/all.hpp"
+
+int main(int argc, char const *argv[]) {
+  auto hello_cmd = opz::new_cmd("hello", "1.0")
+                     .intro("Greeting people since the dawn of computing")
+                     .pos<"name">({.help = "Your name please, so I can greet you"})
+                     .flg<"help", "h">(opz::default_help)
+                     .flg<"version", "v">(opz::default_version);
+
+  auto const map = opz::parse(hello_cmd, argc, argv);
+  auto const name = map.get<"name">();
+  std::print("Hello, {}!\n", name);
+}
