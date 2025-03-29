cc364f3d5bb27fd36b824ffbe8a9bb2815caba81
Gabriel Galli <ggabriel96@hotmail.com>
2025-03-28 21:55:45 -0300

    feat: make sub variadic

--- include/opzioni/cmd.hpp
+++ include/opzioni/cmd.hpp
@@ -71,8 +71,8 @@ struct Cmd<StringList<Names...>, TypeList<Types...>, SubCmds...> {
       args(std::tuple_cat(other.args, std::make_tuple(new_arg))),
       subcmds(other.subcmds) {}
 
-  template <concepts::Cmd OtherCmd, concepts::Cmd NewSubCmd>
-  explicit consteval Cmd(OtherCmd const &other, NewSubCmd const &new_subcmd)
+  template <concepts::Cmd OtherCmd, concepts::Cmd... NewSubCmds>
+  explicit consteval Cmd(OtherCmd const &other, NewSubCmds const &...new_subcmds)
     : name(other.name),
       version(other.version),
       introduction(other.introduction),
@@ -80,7 +80,7 @@ struct Cmd<StringList<Names...>, TypeList<Types...>, SubCmds...> {
       error_handler(other.error_handler),
       amount_pos(other.amount_pos),
       args(other.args),
-      subcmds(std::tuple_cat(other.subcmds, std::make_tuple(std::cref(new_subcmd)))) {}
+      subcmds(std::tuple_cat(other.subcmds, std::make_tuple(std::cref(new_subcmds)...))) {}
 
   consteval auto &intro(std::string_view intro) {
     if (!is_valid_intro(intro))
@@ -101,9 +101,9 @@ struct Cmd<StringList<Names...>, TypeList<Types...>, SubCmds...> {
     return *this;
   }
 
-  template <concepts::Cmd NewSubCmd>
-  consteval auto sub(NewSubCmd const &subcmd) const noexcept {
-    Cmd<StringList<Names...>, TypeList<Types...>, SubCmds..., NewSubCmd> new_cmd(*this, subcmd);
+  template <concepts::Cmd... NewSubCmds>
+  consteval auto sub(NewSubCmds const &...subcmds) const noexcept {
+    Cmd<StringList<Names...>, TypeList<Types...>, SubCmds..., NewSubCmds...> new_cmd(*this, subcmds...);
     return new_cmd;
   }
 
